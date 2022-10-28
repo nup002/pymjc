12fc3661bb1ca0e491cdcce366b974ee44718152
magla <mag.lauritzen@gmail.com>
2022-10-28 22:34:36 +0200

    First commit of stub file

---
+++ mjc/mjc.pyi
@@ -0,0 +1,25 @@
+import numpy as np
+import numpy.typing as npt
+from typing import Union, Tuple, List
+
+def minimum_mjc(s1: Union[List, npt.NDArray[Union[np.integer, np.floating]]],
+                s2: Union[List, npt.NDArray[Union[np.integer, np.floating]]],
+                dxy_limit: float = np.inf,
+                beta: float = 1.,
+                show_plot: bool = False,
+                std_s1: float = None, std_s2: float = None,
+                tavg_s1: float = None, tavg_s2: float = None,
+                override_checks=False) -> Tuple[float, bool]: ...
+
+def mjc(s1: Union[List, npt.NDArray[Union[np.integer, np.floating]]],
+        s2: Union[List, npt.NDArray[Union[np.integer, np.floating]]],
+        dxy_limit: float = np.inf,
+        beta: float = 1.,
+        show_plot: bool = False,
+        std_s1: float = None, std_s2: float = None,
+        tavg_s1: float = None, tavg_s2: float = None,
+        return_args=False,
+        override_checks=False) -> Union[Tuple[float, bool],
+                                        Tuple[float, bool, float, float, float, float,
+                                              npt.NDArray[Union[np.integer, np.floating]],
+                                              npt.NDArray[Union[np.integer, np.floating]]]]: ...
